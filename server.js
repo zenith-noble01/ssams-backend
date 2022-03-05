f5c048949f35de66e60f68539a43bf94262cbb9a
zenith-noble01 <webdev686@gmail.com>
2022-03-05 10:54:00 +0100

    made the authentication

--- server.js
+++ server.js
@@ -11,14 +11,20 @@ const morgan = require("morgan");
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+  })
+);
 app.use(express.json());
 connectDb();
 // app.use(notFound);
 app.use(errorHandler);
 app.use(morgan("dev"));
 
-app.use("/api/auth", require("./routes/auth"));
+app.use("/api/parent/auth", require("./routes/auth"));
 const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
